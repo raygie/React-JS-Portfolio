ca45c5219074007542081382be34d0ddd3d5b8a4
raygie <raygieramos18@gmail.com>
2022-07-02 12:31:08 +0800

    edit h2 in hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -25,7 +25,7 @@ const Hero = () => {
                     <div className='hero_content'>
                         <h4>Welcome to My Personal Website</h4>
                         <h1>Hi, I'm <span>Raygie Ramos</span></h1>
-                        <h1 className='hero_effect mb-4'>a <span ref={textRef}></span></h1>
+                        <h2 className='hero_effect mb-4'>a <span ref={textRef}></span></h2>
                         <p className='hero_p2 mt-4'>I use animation as a third dimension by which to simplify experiences and kuiding thro each and every interaction. I’m not adding motion just to spruce things up, but doing it in ways that.</p>
                         <div className="hero_btns d-flex align-items-center gap-4 mt-5">
                             <button className='bn works_btn'><a href='#project'>See My Works</a></button>
